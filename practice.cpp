01cc0e0090dbb85d68a4dff0aad08fd09500d2d5
Harjas-Sidhu <harjassidhu1@gmail.com>
2023-09-12 19:47:29 +0530

    modified:   practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -2,41 +2,33 @@
 #include <vector>
 using namespace std;
 
-vector <int> divisors_of(int n){
-    vector <int> result = {-1};
-    for (int i = 2; i < n; i++)
+vector <long long unsigned int> divisors_of(long long unsigned int n){
+    vector <long long unsigned int> result = {0};
+    for (long long unsigned int i = 2; i < n; i++)
     {
         if(n%i == 0){
-            result.pop_back();
+            if(result[result.size()-1] == 0){
+                result.pop_back();
+            }
             result.push_back(i);
         }
     }
     return result;
 }
-class Class1{
-    private:
-        int pass;
-    public:
-        void set_pass(int new_pass){
-            pass = new_pass;
-        }
-        int get_pass(){
-            return pass;
-        }
-};
 
-int main(){
-    Class1 Object1;
-    Object1.set_pass(100);
-    cout << Object1.get_pass() << endl;
-    int n;
-    cin >> n;
-    vector <int> res = divisors_of(n);
-    vector <int> :: iterator i;
+void print(vector <long long unsigned int> res){
+    vector <long long unsigned int> :: iterator i;
     for (i = res.begin(); i != res.end(); i++)
     {
-        cout << *i << endl;
+        cout<<*i<<" ";
     }
-    
+    cout<<endl;
+}
+
+int main(){
+    long long unsigned int n;
+    cin >> n;
+    vector <long long unsigned int> res = divisors_of(n);
+    print(res);
     return 0;
 }
